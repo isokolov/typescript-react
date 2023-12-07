689b763b81e456fa4174c3bec685f07ea926860c
isokolov <illya.sokolov82@gmail.com>
2023-12-07 12:39:20 +0200

    002-primitives

--- typescript-training/index.js
+++ typescript-training/index.js
@@ -1,2 +1,6 @@
-var awesome = 'Its going to be cool';
+// By initialization implicit type reference
+var awesome = 'This is going to be cool';
+awesome.toLocaleLowerCase();
+var value = 1;
+var isCool = true;
 document.getElementsByTagName('p')[0].innerText = awesome;
--- typescript-training/index.ts
+++ typescript-training/index.ts
@@ -1,3 +1,9 @@
-const awesome = 'Its going to be cool';
+// By initialization implicit type reference
+let awesome: string = 'This is going to be cool';
+
+awesome.toLocaleLowerCase();
+let value: number = 1;
+
+let isCool: boolean = true;
 
 document.getElementsByTagName('p')[0].innerText = awesome;
