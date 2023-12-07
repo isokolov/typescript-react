cd676caf39d7d637d2106581fd26ca8a2be8a20b
isokolov <illya.sokolov82@gmail.com>
2023-12-07 20:46:57 +0200

    006-interfaces

--- typescript-training/index.js
+++ typescript-training/index.js
@@ -1,12 +1,19 @@
+var _a, _b;
 var persons = [
     {
         name: 'Alice',
         job: 'Font-end Dev',
         age: 28,
+        isHappy: true,
+        friendList: [{ age: 30, job: 'Astronaut', name: 'Tobby' }],
+        sayHi: function (color) {
+            console.log('Hi i am Alice ! my favorite color is ', color);
+        },
     },
     { name: 'Bob', job: 'Back-end dev', age: 44 },
     { name: 'Codiku', job: 'Full Stack dev', age: 30 },
 ];
+(_b = (_a = persons[0]).sayHi) === null || _b === void 0 ? void 0 : _b.call(_a, 'red');
 var formatedList = persons.map(function (person) {
     return "<li>Name : ".concat(person.name, " - Job : ").concat(person.job, " - Age : ").concat(person.age, "</li>");
 });
--- typescript-training/index.ts
+++ typescript-training/index.ts
@@ -1,22 +1,32 @@
-const persons: {
+interface Person {
   name: string;
   job: string;
   age: number;
-}[] = [
+  isHappy?: boolean;
+  friendList?: Person[];
+  sayHi?: (color: string) => void;
+}
+
+const persons: Person[] = [
   {
     name: 'Alice',
     job: 'Font-end Dev',
     age: 28,
+    isHappy: true,
+    friendList: [{ age: 30, job: 'Astronaut', name: 'Tobby' }],
+    sayHi: (color: string) => {
+      console.log('Hi i am Alice ! my favorite color is ', color);
+    },
   },
   { name: 'Bob', job: 'Back-end dev', age: 44 },
   { name: 'Codiku', job: 'Full Stack dev', age: 30 },
 ];
 
-const formatedList: string[] = persons.map(
-  (person: { name: string; job: string; age: number }): string => {
-    return `<li>Name : ${person.name} - Job : ${person.job} - Age : ${person.age}</li>`;
-  }
-);
+persons[0].sayHi?.('red');
+
+const formatedList: string[] = persons.map((person: Person): string => {
+  return `<li>Name : ${person.name} - Job : ${person.job} - Age : ${person.age}</li>`;
+});
 
 const liListString: string = formatedList.join('');
 
